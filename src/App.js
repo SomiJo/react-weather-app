bd0ffa1a13e04c43b90f8bcc84d72a2fdb1c21c1
SomiJo <jodeirsomaye@gmail.com>
2023-10-27 10:50:08 +0500

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1> Weather App</h1>
+      <footer>
+        This App was coded by Somaye Jodeir and is{" "}
+        <a
+          href="https://github.com/SomiJo/react-weather-app.git"
+          target="_blank">
+          open_source on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;

3db9ccb71ac0d150a9896c3663ed6f4f1e062c83
SomiJo <jodeirsomaye@gmail.com>
2023-10-27 11:13:57 +0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {" "}
-      <h1> Weather App</h1>
-      <footer>
-        This App was coded by Somaye Jodeir and is{" "}
-        <a
-          href="https://github.com/SomiJo/react-weather-app.git"
-          target="_blank">
-          open_source on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This App was coded by Somaye Jodeir and is{" "}
+          <a
+            href="https://github.com/SomiJo/react-weather-app.git"
+            target="_blank">
+            open_source on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
